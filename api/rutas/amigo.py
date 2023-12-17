25ef675f552d88b4b948e0b0f70842608d34c19f
Facundo José García Gallo <alu0101406329@ull.edu.es>
2023-12-17 09:47:12 +0000

    Finalización de la implementación de amigo

--- api/rutas/amigo.py
+++ api/rutas/amigo.py
@@ -32,3 +32,52 @@ def get_amigos(username):
     print("Ha ocurrido un error", e)
     return jsonify({"message": "Ha ocurrido un error"}), 500
 
+
+#########################################################################################################################################
+
+
+# recibimos parametros por el body de la peticion
+@amigo_bp.route('', methods=['POST'])
+def post_amigo():
+  try:
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    data = request.get_json()
+    cursor.execute(f"INSERT INTO amigo (username1, username2) VALUES ('{data['username1']}', '{data['username2']}');")
+    cursor.execute(f"SELECT * FROM amigo WHERE username1 = '{data['username1']}' AND username2 = '{data['username2']}';")
+    amigos = cursor.fetchone()
+    amigos = {
+      "username1": amigos[0],
+      "username2": amigos[1]
+    }
+    conn.commit()
+    cursor.close()
+    conn.close()
+    # retonarmos el mensaje de exito y el usuario creado
+    return jsonify({"message": "Amigos añadidos exitosamente", "amigos": amigos}), 201
+  except Exception as e:
+    return jsonify({"message": "Ha ocurrido un error", "error": f"Errores con {e}"}), 500
+
+
+
+#########################################################################################################################################
+
+
+@amigo_bp.route('<string:username>', methods=['DELETE'])
+def delete_amigo(username):
+  try:
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    data = request.get_json()
+
+    cursor.execute(f"DELETE FROM amigo WHERE username1 = '{data['username']}' AND username2 = '{username}';")
+    cursor.execute(f"DELETE FROM amigo WHERE username1 = '{username}' AND username2 = '{data['username']}';")
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return jsonify({"message": "Amigos eliminados con éxito"}), 200
+  except Exception as e:
+    print("Ha ocurrido un error", e)
+    return jsonify({"message": "Ha ocurrido un error"}), 500
