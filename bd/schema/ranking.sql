473299dd0628cac4c70817b18f3268f22f4906ac
Facundo José García Gallo <alu0101406329@ull.edu.es>
2023-12-18 11:28:49 +0000

    Corrección de errores menores

--- bd/schema/ranking.sql
+++ bd/schema/ranking.sql
@@ -2,3 +2,6 @@ CREATE TABLE ranking (
   username TEXT,
   n_recetas INTEGER
 );
+
+ALTER TABLE ONLY public.ranking
+  ADD CONSTRAINT username_fkey FOREIGN KEY (username) REFERENCES public.usuario(username) ON UPDATE CASCADE ON DELETE CASCADE;
